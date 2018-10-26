b6cecc924f2e301bdc85d42d24bf281b2f009132
Dmitriy Pyntikov <pyntikovd@gmail.com>
2018-10-26 19:03:29 +0300

    FirstCommit

---
+++ DEV3/DEV3/ConvertToAnyRadix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DEV3
+{
+    //Converts the number in other system of notation
+    public class ConvertToAnyRadix
+    {
+        // "decimalNumber"  input decimal number
+        // "radix"> new number notation
+        // output number of the new system 
+        public string Convert(int decimalNumber, int radix)
+        {
+            string convertedValue = "";
+            do
+            {
+                int remainder = decimalNumber % radix;
+                decimalNumber = decimalNumber / radix;
+                radix = ChooseSymbol(remainder) + radix;
+            }
+            while (decimalNumber > 0);
+            return convertedValue;
+        }
+        private char ChooseSymbol(int remainder)
+        {
+            const string symbols = "0123456789ABCDEFGHIJ";
+            return symbols[remainder];
+        }
+
+    }
+
+}
+
+
+
---
+++ DEV3/DEV3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV3
+{
+    //point of entry
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int decimalNumber;
+            int radix;
+            const int minRadix = 2;
+            const int maxRadix = 20;
+            if (args.Length != minRadix)
+            {
+                Console.WriteLine("Incorrect format of the input data! Try again!");
+                return;
+            }
+            try
+            {
+                decimalNumber = Int32.Parse(args[0]);
+                radix = Int32.Parse(args[1]);
+                if (radix > minRadix && radix < maxRadix)
+                {
+                    ConvertToAnyRadix converter = new ConvertToAnyRadix();
+                    Console.WriteLine("Ouput: {0}", converter.Convert(decimalNumber, radix));
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect format of the input data!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
